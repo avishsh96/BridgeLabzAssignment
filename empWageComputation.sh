061f1cac1b416b6963c6db767ef71e3c0eee0f9e
Avinash <sharma.avinash96@gmail.com>
2020-08-04 23:56:44 +0530

    Added part time wage functionality using if else

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -x
-echo "Welcome to Employee wage Computation Program on Master Branch"
 
 Wage_Per_Hr=20
+Part_Time_Wage_Per_Hr=8
 Full_Day_Hr=8
- 
 empCheck=$((RANDOM%2))
+isParttime=1
+isFulltime=1
+randomCheck=$((RANDOM%2))
+
 
 if [[ empCheck -eq 1 ]]
 then
-   dailyWage=$(($Wage_Per_Hr*Full_Day_Hr))
-   echo "employee is present" "Wage: "$dailyWage
+   echo "employee is present"
+	if [[ $isFulltime -eq $randomCheck ]]
+	then
+		echo "Employee is Fulltime Wage: " $(($Wage_Per_Hr*$Full_Day_Hr))
+	else
+	echo "Emplpyee is Part time Wage: " $(($Part_Time_Wage_Per_Hr*$Full_Day_Hr))
+	fi
 else
    echo "employee is absent"
 fi
