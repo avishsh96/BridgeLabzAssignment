5463643cc9407efeb2dcbcf6d4d305a1ed1d97fb
Avinash <sharma.avinash96@gmail.com>
2020-08-04 23:17:58 +0530

    Added Random function empcheck

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
+
+
+empCheck=$((RANDOM%2))
+
 if [[ empCheck -eq 1 ]]
 then
-        echo "employee is present"
+	echo "employee is present"
 else
-        echo "employee is absent"
+	echo "employee is absent"
 fi
