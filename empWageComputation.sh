24e2991cbeabeb34a259861f49c9c2f0eafdcf97
Avinash <sharma.avinash96@gmail.com>
2020-08-04 23:25:24 +0530

    added daily empployee Wage functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,10 +4,11 @@ echo "Welcome to Employee wage Computation Program on Master Branch"
 Wage_Per_Hr=20
 Full_Day_Hr=8
 
+empCheck=$((RANDOM%2))
 
 if [[ empCheck -eq 1 ]]
 then
-	dailyWage$(($Wage_Per_Hr*Full_Day_Hr))
+	dailyWage=$(($Wage_Per_Hr*Full_Day_Hr))
 	echo "employee is present" "Wage: "$dailyWage
 else
    echo "employee is absent"
