4f8a99b0da1c90abda6062bdda8c880496494cba
Avinash <sharma.avinash96@gmail.com>
2020-08-05 15:32:26 +0530

    solving using Switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,23 +1,21 @@
 #!/bin/bash -x
 
 Wage_Per_Hr=20
-Part_Time_Wage_Per_Hr=8
-Full_Day_Hr=8
-empCheck=$((RANDOM%2))
-isParttime=1
-isFulltime=1
-randomCheck=$((RANDOM%2))
+empCheck=$((RANDOM%3))
+isPartTime=1
+isFullTime=2
 
 
-if [[ empCheck -eq 1 ]]
-then
-   echo "employee is present"
-	if [[ $isFulltime -eq $randomCheck ]]
-	then
-		echo "Employee is Fulltime Wage: " $(($Wage_Per_Hr*$Full_Day_Hr))
-	else
-	echo "Emplpyee is Part time Wage: " $(($Part_Time_Wage_Per_Hr*$Full_Day_Hr))
-	fi
-else
-   echo "employee is absent"
-fi
+case $empCheck in
+	$isPartTime)
+		empHours=4
+	;;
+	$isFullTime)
+		empHours=8
+	;;
+	*)
+		empHours=0
+	;;
+esac
+totalSalary=$(($Wage_Per_Hr*$empHours))
+echo "totalSalry: "$totalSalary
